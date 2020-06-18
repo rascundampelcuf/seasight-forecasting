8e95d000e84cb1c93b0dd8626cdc3012e02998b9
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-06-18 12:00:44 +0200

    :zap: Modified sockets method to read cleaner data

--- src/Sockets.py
+++ src/Sockets.py
@@ -7,14 +7,15 @@ Created on Mon Jun  8 17:40:29 2020
 
 import socket
 
-# UDP_IP = "192.168.128.136"
 UDP_IP = "0.0.0.0"
 UDP_PORT = 21567
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
+    counter, latitude, longitude, altitude, heading, tilt, roll, time_start, time_end, planet_name  = data.decode("utf-8").split(',')    
+    print("received message: {} - {}".format(latitude, longitude))
