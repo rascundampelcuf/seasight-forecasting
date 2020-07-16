49ffaefaeed506b772ee5b273017dddc0037a123
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-16 14:16:37 +0200

    :bug: Fixed action form urls

--- django/seasight_forecasting/urls.py
+++ django/seasight_forecasting/urls.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jul  7 11:49:55 2020
-
-@author: gizqu
-"""
 
 from django.conf.urls import include, url
 from django.urls import path
@@ -11,10 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/test', views.app, name='app'),
-    url(r'^past/$', views.past, name='past'),
-    url(r'^present/$', views.present, name='present'),
-    url(r'^future/$', views.future, name='future'),
+    path('test', views.app, name='app'),
+    path('past/', views.past, name='past'),
+    path('present/', views.present, name='present'),
+    path('future/', views.future, name='future'),
     url(r'^submit/', views.submit),
-    url(r'^past/generateHistoricKML/', views.generateHistoricKML, name='generateHistoricKML'),
 ]
