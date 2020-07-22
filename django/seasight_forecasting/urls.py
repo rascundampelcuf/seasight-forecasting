d94b3c7936675c358242b3be90c10a118b66c74f
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-22 10:35:27 +0200

    :fire: Removed useless code

--- django/seasight_forecasting/urls.py
+++ django/seasight_forecasting/urls.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import include, url
 from django.urls import path
 from . import views
 
@@ -10,5 +9,4 @@ urlpatterns = [
     path('past/', views.past, name='past'),
     path('present/', views.present, name='present'),
     path('future/', views.future, name='future'),
-    url(r'^submit/', views.submit),
 ]
