e896e7edcb1069a8285d0241d6160d9471c7e58d
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-22 10:32:37 +0200

    :fire: Removed useless code

--- django/seasight_forecasting/GetBounds.py
+++ django/seasight_forecasting/GetBounds.py
@@ -2,16 +2,14 @@
 
 from random import randint
 
-def GetBounds(alt, lat, lon):
-    label = 'Alt {}_Lat {} Lon_{}'.format(alt, lat, lon)
-    
+def GetBounds(alt, lat, lon):    
     width = int(alt) * 25 / 2000
     height = int(alt) * 15 / 2000
     left = int(lon) - width
     right = int(lon) + width
     up = int(lat) + height
     down = int(lat) - height
-    return left, right, up, down, label
+    return left, right, up, down
 
 def GetDataFromBounds(data, left, right, up, down):
-    return data[(data['LAT'].between(down, up)) & (data['LON'].between(left, right))]
+    return data[(data['lat'].between(down, up)) & (data['lon'].between(left, right))]
