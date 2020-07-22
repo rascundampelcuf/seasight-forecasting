eaf405f08a15a89d720e577053f8ed504f133174
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-22 13:32:17 +0200

    :bug: Fixed variables name bug

--- django/seasight_forecasting/GenerateKMLMethods.py
+++ django/seasight_forecasting/GenerateKMLMethods.py
@@ -16,9 +16,9 @@ def GenerateHistoricKML(region, dateFrom, check, dateTo, alt, lat, lon):
     data = LoadData('../data/past.csv')
     data.time = pd.to_datetime(data.time, errors='coerce', format='%Y-%m-%d %H:%M:%S')
     data = GetDataInDateRange(data, dateFrom, check, dateTo)
-    data.nlon = pd.to_numeric(data.lon, downcast="float")
-    data.nlat = pd.to_numeric(data.lat, downcast="float")
-    data.analysed_sst = pd.to_numeric(data.sst, downcast="float")
+    data.lon = pd.to_numeric(data.lon, downcast="float")
+    data.lat = pd.to_numeric(data.lat, downcast="float")
+    data.sst = pd.to_numeric(data.sst, downcast="float")
     data = GetDataFromRegion(data, region)
     # alt, lat, lon = GetParameters()
     #left, right, up, down = GetBounds(alt, lat, lon)
@@ -26,11 +26,10 @@ def GenerateHistoricKML(region, dateFrom, check, dateTo, alt, lat, lon):
     #try:
     regions = []
     for group in data.groupby(['time']):
-        time = group[1]['time']
-        ngroup = group[1].drop(['time'], axis=1)
-        ngroup = GetClusters(n_clusters, ngroup)
-        ngroup['time'] = time
-        regions.append(GetRegions(n_clusters, ngroup, InitCmap(data['sst']), True))
+        data = group[1]
+        data = data.drop(['time'], axis=1)
+        ngroup = GetClusters(n_clusters, data)
+        regions.append(GetRegions(n_clusters, ngroup, InitCmap(data['sst']), group[0].date()))
     path = 'seasight_forecasting/static/seasight_forecasting/kml/SST_regions.kml'
     CreateKML(regions, path, True)
     message = 'Created KML file in {}'.format(path)
@@ -41,8 +40,8 @@ def GenerateHistoricKML(region, dateFrom, check, dateTo, alt, lat, lon):
 def GenerateRealTimeKML(region, alt, lat, lon):
     n_clusters = 200
     data = LoadData('../data/present.csv')
-    data.nlon = pd.to_numeric(data.lon, downcast="float")
-    data.nlat = pd.to_numeric(data.lat, downcast="float")
+    data.lon = pd.to_numeric(data.lon, downcast="float")
+    data.lat = pd.to_numeric(data.lat, downcast="float")
     data = data.drop(['time'], axis=1)
     data = GetDataFromRegion(data, region)
     # alt, lat, lon = GetParameters()
@@ -61,8 +60,8 @@ def GenerateRealTimeKML(region, alt, lat, lon):
 def GenerateFutureKML(region, alt, lat, lon):
     n_clusters = 200
     data = LoadData('../data/future.csv')    
-    data.nlon = pd.to_numeric(data.lon, downcast="float")
-    data.nlat = pd.to_numeric(data.lat, downcast="float")
+    data.lon = pd.to_numeric(data.lon, downcast="float")
+    data.lat = pd.to_numeric(data.lat, downcast="float")
     data = data.drop(['time'], axis=1)
     data = GetDataFromRegion(data, region)
     # alt, lat, lon = GetParameters()
