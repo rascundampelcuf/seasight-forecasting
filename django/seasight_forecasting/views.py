7d3f16fecbf49e0f39e5264c6af9085f20709200
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-22 10:36:11 +0200

    :zap: Fixed submits from view site

--- django/seasight_forecasting/views.py
+++ django/seasight_forecasting/views.py
@@ -2,7 +2,7 @@ import subprocess
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from seasight_forecasting.GenerateHistoricKML import *
+from seasight_forecasting.GenerateKMLMethods import *
 
 def index(request):
     return render(request, 'seasight_forecasting/index.html', {})
@@ -12,35 +12,40 @@ def app(request):
 
 def past(request):
     if request.method == 'GET':
-        context = {}
+        min_date, max_date = GetDate()
+        context = {'min_date': min_date, 'max_date': max_date}
     else:
         region = request.POST.get('region')
         dateFrom = request.POST.get('dateFrom')
-        check = request.POST.get('check')    
+        check = request.POST.get('check')
         dateTo = request.POST.get('dateTo')
         alt = request.POST.get('altitude')
         lat = request.POST.get('latitude')
         lon = request.POST.get('longitude')
         message = GenerateHistoricKML(region, dateFrom, check, dateTo, alt, lat, lon)
-        #ctypes.windll.user32.MessageBoxW(0, message, "Your title", 0)
-        context = {'response': message, 'ok': 1, 'fail': 0}
+        context = {'response': message}
     return render(request, 'seasight_forecasting/past.html', context)
 
 def present(request):
-    return render(request, 'seasight_forecasting/present.html', {})
+    if request.method == 'GET':
+        context = {}
+    else:
+        region = request.POST.get('region')
+        alt = request.POST.get('altitude')
+        lat = request.POST.get('latitude')
+        lon = request.POST.get('longitude')
+        message = GenerateRealTimeKML(region, alt, lat, lon)
+        context = {'response': message}
+    return render(request, 'seasight_forecasting/present.html', context)
 
 def future(request):
-    return render(request, 'seasight_forecasting/future.html', {})
-
-def submit(request):
-    alt = request.POST.get('altitude')
-    lat = request.POST.get('latitude')
-    lon = request.POST.get('longitude')
-    n_clusters = 100
-    data = LoadData('../data/dummy_data2.csv')
-    left, right, up, down, message = GetBounds(alt, lat, lon)
-    data = GetDataFromBounds(data, left, right, up, down)
-    data = GetClusters(n_clusters, data)
-    regions = GetRegions(n_clusters, data, InitCmap(data.SST))
-    CreateKML(regions, '../data/final KMLs/SST_regions-' + message + '.kml')
-    return HttpResponseRedirect('/')
+    if request.method == 'GET':
+        context = {}
+    else:
+        region = request.POST.get('region')
+        alt = request.POST.get('altitude')
+        lat = request.POST.get('latitude')
+        lon = request.POST.get('longitude')
+        message = GenerateFutureKML(region, alt, lat, lon)
+        context = {'response': message}
+    return render(request, 'seasight_forecasting/future.html', context)
