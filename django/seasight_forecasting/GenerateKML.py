5a228d847b89c69a1bf195e1c5c4ff368ea33db1
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-22 13:32:48 +0200

    :boom: KML sequence creation

--- django/seasight_forecasting/GenerateKML.py
+++ django/seasight_forecasting/GenerateKML.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Import libraries
+import itertools
 from lxml import etree
 from pykml.factory import KML_ElementMaker as KML
 
@@ -10,18 +11,31 @@ def GetCoords(region):
         string += '{},{},0\n'.format(p[0], p[1])
     return string
 
-def CreateKML(regions, path):
+def CreateKML(regions, path, withDate):
     fich_kml = KML.kml(
-        KML.Document(        
+        KML.Document(
             KML.Folder(
                 KML.name('Temperature regions'))
             )
         )
-    
+    if withDate:
+        regions = list(itertools.chain.from_iterable(regions))        
+    #print(regions)
     for region in regions:
+        date_from = ''
+        date_to = ''
+        if withDate:
+            dates = region.pop()
+            date_from = dates[0]
+            date_to = dates[1]
         color = region.pop()
+
         fich_kml.Document.Folder.append(
             KML.Placemark(
+                KML.TimeSpan(
+                    KML.begin(date_from),
+                    KML.end(date_to)
+                ),
                 KML.Style(
                 KML.PolyStyle(
                     KML.color(color),
