1229112b2416c45c2ba3dc75c97b3329ed31d4bc
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-22 13:34:38 +0200

    :sparkles: Added date when create clusters

--- django/seasight_forecasting/Clustering.py
+++ django/seasight_forecasting/Clustering.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # Import libraries
+import datetime
 import math
 import matplotlib as mpl
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from scipy.spatial import ConvexHull
 from sklearn.cluster import AgglomerativeClustering
-
-def LoadData(data_path):    
-    return pd.read_csv(data_path)
 
 def rgb_to_hex(rgb):
     return 'ff%02x%02x%02x' % (int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255))
@@ -37,11 +34,11 @@ def GetClusters(n_clusters, data):
     data['labels'] = cluster.labels_
     return data
 
-def GetRegions(n_clusters, data, cmap):
+def GetRegions(n_clusters, data, cmap, date):
     regions = []
     for cluster in range(n_clusters):
         points = data[data.labels == cluster]    
-        point_cloud = points[['LON', 'LAT']].to_numpy()
+        point_cloud = points[['lon', 'lat']].to_numpy()
         hull = ConvexHull(point_cloud)
         # Get first point from ConvexHull lines in order to create a polygon
         pts = np.vstack([point_cloud[x] for x in hull.simplices])
@@ -53,7 +50,11 @@ def GetRegions(n_clusters, data, cmap):
         # Copied the first element in the last position in order to close the circle
         region.append(region[0])
         # Convert the mean temperature value into RGB values into HEX value
-        region.append(rgb_to_hex(cmap.to_rgba(points.SST.mean())))
+        region.append(rgb_to_hex(cmap.to_rgba(points['sst'].mean())))
+        if date:
+            date_from = date
+            date_to = date_from + datetime.timedelta(days=1)
+            region.append([str(date_from), str(date_to)])
         regions.append(region)
     return regions
 
