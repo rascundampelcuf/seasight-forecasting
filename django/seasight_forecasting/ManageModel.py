5d6a0517bd8dceffe13de04fe0a89c580d94d45d
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-27 15:14:43 +0200

    :zap: Created model to load RNN model and load data

---
+++ django/seasight_forecasting/ManageModel.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import model_from_json
+
+def LoadRNNModel():
+    json_file = open('../model/model2.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("../model/model2.h5")
+    loaded_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    return loaded_model
+
+def NewPrediction(data, model):
+    ndf = pd.DataFrame(columns=['lat','lon','sst'])
+    df = data.groupby(['lat','lon'])['sst'].apply(list)
+    for lat, lon, sst_list in df:
+        if len(sst_list) >= 2:
+            pred = model.predict(sst_list[:2])
+            ndf.append({'lat': lat, 'lon': lon, 'sst': pred}, ignore_index = True)
+
+    new_predictions = model.predict(x_test)
+    return data
