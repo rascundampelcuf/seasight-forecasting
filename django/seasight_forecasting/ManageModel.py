1ede604a63a082de9eeab7569587833cc82505eb
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-07-28 13:06:16 +0200

    :sparkles: Created method to get predicted data

--- django/seasight_forecasting/ManageModel.py
+++ django/seasight_forecasting/ManageModel.py
@@ -1,4 +1,5 @@
 
+import pickle
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import model_from_json
@@ -12,11 +13,31 @@ def LoadRNNModel():
     loaded_model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
     return loaded_model
 
+def LoadLRModel():
+    # load the model from disk
+    loaded_model = pickle.load(open('../model/lr_model.pkl', 'rb'))
+    return loaded_model
+
 def NewPrediction(data, model):
     ndf = pd.DataFrame(columns=['lat','lon','sst'])
-    df = data.groupby(['lat','lon'])['sst'].apply(list)
-    for lat, lon, sst_list in df:
-        if len(sst_list) >= 2:
-            pred = model.predict(sst_list[:2])
-            ndf.append({'lat': lat, 'lon': lon, 'sst': pred}, ignore_index = True)
+    df = data.groupby(['lat','lon'])['sst'].apply(list).reset_index()
+    for _, row in df.iterrows():
+        if len(row.sst) >= 2:
+            val = np.array([[row.sst[:2]]])
+            print(val)
+            pred = model.predict(val)
+            print(pred)
+            ndf.append({'lat': row.lat, 'lon': row.lon, 'sst': pred}, ignore_index = True)
+    return ndf
+
+def LRPrediction(data, model):
+    ndf = pd.DataFrame(columns=['lat','lon','sst'])
+    df = data.groupby(['lat','lon'])['sst'].apply(list).reset_index()
+    for _, row in df.iterrows():
+        a = np.array(row.sst).reshape(-1,1)
+        pred = model.predict(a)
+        nr = pd.DataFrame({'lat': row.lat, 'lon': row.lon, 'sst': pred})
+        print(nr)
+        ndf = ndf.append(nr, ignore_index = True)
+    print(ndf)
     return ndf
