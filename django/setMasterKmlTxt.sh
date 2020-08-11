4d73e300094bdc4b703fa1e2853f99449cdfddad
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-08-11 11:44:45 +0200

    :tada: Script to write kmls.txt from server DONE

--- django/setMasterKmlTxt.sh
+++ django/setMasterKmlTxt.sh
@@ -1,4 +1,4 @@
-$ip = ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'
-$file = '/var/www/html/kmls.txt'
+ip=`ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+file='/var/www/html/kmls.txt'
 
-ssh $1 "sed 'http://$ip:81/static/kml/SST_regions.kml' $file"
+ssh $1 "echo 'http://$ip:81/static/kml/SST_regions.kml' > $file"
