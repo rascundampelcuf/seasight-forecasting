a6252c394a15d94cf25c85b14f4829c445e017bb
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-08-13 18:53:05 +0200

    :bug: Fixed bug in bash script

--- django/setMasterKmlTxt.sh
+++ django/setMasterKmlTxt.sh
@@ -30,10 +30,11 @@ ssh $master_ip "
                 <overlayXY x=\"0\" y=\"1\" xunits=\"fraction\" yunits=\"fraction\"/>
                 <screenXY x=\"0.02\" y=\"0.9\" xunits=\"fraction\" yunits=\"fraction\"/>
                 <rotationXY x=\"0\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"/>
-                <size x=\"0.1\" y=\"0.2\" xunits=\"fraction\" yunits=\"fraction\"/>
+                <size x=\"0.5\" y=\"0.5\" xunits=\"fraction\" yunits=\"fraction\"/>
             </ScreenOverlay>
         </Folder>
-    </Document>' > $logos_file_target
+    </Document>
+</kml>' > $logos_file_target
     echo '<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\">
     <Document>
         <Folder>
@@ -44,10 +45,11 @@ ssh $master_ip "
                 <href>http://$server_ip:8000/static/img/colorbar.png</href>
                 </Icon>
                 <overlayXY x=\"0\" y=\"1\" xunits=\"fraction\" yunits=\"fraction\"/>
-                <screenXY x=\"0.85\" y=\"0.3\" xunits=\"fraction\" yunits=\"fraction\"/>
+                <screenXY x=\"0.85\" y=\"0.8\" xunits=\"fraction\" yunits=\"fraction\"/>
                 <rotationXY x=\"0\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"/>
                 <size x=\"0.1\" y=\"0.6\" xunits=\"fraction\" yunits=\"fraction\"/>
             </ScreenOverlay>
         </Folder>
-    </Document>' > $colorbar_file_target
+    </Document>
+</kml>' > $colorbar_file_target
 "
