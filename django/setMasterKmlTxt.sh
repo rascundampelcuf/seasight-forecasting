48911a9b32a9de0829d70c069c3404540eccb561
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-08-15 10:31:51 +0200

    :fire: Removed sending colorbarKML in the setup sh file

--- django/setMasterKmlTxt.sh
+++ django/setMasterKmlTxt.sh
@@ -4,8 +4,6 @@ master_ip=$(awk -F "=" '/master_IP/ {print $2}' app.conf)
 master_ip=${master_ip// /}
 screen_for_logos=$(awk -F "=" '/screen_for_logos/ {print $2}' app.conf)
 screen_for_logos=${screen_for_logos// /}
-screen_for_colorbar=$(awk -F "=" '/screen_for_colorbar/ {print $2}' app.conf)
-screen_for_colorbar=${screen_for_colorbar// /}
 
 kml_filename=$(awk -F "=" '/kml_destination_file/ {print $2}' app.conf)
 kml_filename=${kml_filename// /}
@@ -14,7 +12,6 @@ kml_file_source='http://'$master_ip':81/SF/'$kml_filename
 
 kml_file_target='/var/www/html/kmls.txt'
 logos_file_target='/var/www/html/kml/slave_'$screen_for_logos'.kml'
-colorbar_file_target='/var/www/html/kml/slave_'$screen_for_colorbar'.kml'
 
 ssh $master_ip "
     echo $kml_file_source > $kml_file_target
@@ -35,21 +32,4 @@ ssh $master_ip "
         </Folder>
     </Document>
 </kml>' > $logos_file_target
-    echo '<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\">
-    <Document>
-        <Folder>
-            <name>Colorbar</name>
-            <ScreenOverlay>
-                <name>Colorbar</name>
-                <Icon>
-                <href>http://$server_ip:8000/static/img/colorbar.png</href>
-                </Icon>
-                <overlayXY x=\"0\" y=\"1\" xunits=\"fraction\" yunits=\"fraction\"/>
-                <screenXY x=\"0.85\" y=\"0.8\" xunits=\"fraction\" yunits=\"fraction\"/>
-                <rotationXY x=\"0\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"/>
-                <size x=\"0.1\" y=\"0.6\" xunits=\"fraction\" yunits=\"fraction\"/>
-            </ScreenOverlay>
-        </Folder>
-    </Document>
-</kml>' > $colorbar_file_target
 "
