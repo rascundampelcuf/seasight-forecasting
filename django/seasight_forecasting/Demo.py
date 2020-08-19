6ad9f450dbb1af148c309185bf783f786a885fbe
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-08-19 08:56:53 +0200

    :sparkles: New changes for the DEMO

--- django/seasight_forecasting/Demo.py
+++ django/seasight_forecasting/Demo.py
@@ -26,26 +26,53 @@ def sendKmlToLG(main, slave):
     print(command)
     os.system(command)
 
+def sendFlyToToLG(lat, lon, altitude, heading, tilt, pRange, duration):
+    flyTo = "flytoview=<LookAt>" \
+            + "<longitude>" + str(lon) + "</longitude>" \
+            + "<latitude>" + str(lat) + "</latitude>" \
+            + "<altitude>" + str(altitude) + "</altitude>" \
+            + "<heading>" + str(heading) + "</heading>" \
+            + "<tilt>" + str(tilt) + "</tilt>" \
+            + "<range>" + str(pRange) + "</range>" \
+            + "<altitudeMode>relativeToGround</altitudeMode>" \
+            + "<gx:altitudeMode>relativeToGround</gx:altitudeMode>" \
+            + "<gx:duration>" + str(duration) + "</gx:duration>" \
+            + "</LookAt>"
+
+    command = "echo '" + flyTo + "' | sshpass -p " + global_vars.lg_pass + " ssh " + global_vars.lg_IP + " 'cat - > /tmp/query.txt'"
+    print(command)
+    os.system(command)
+
 def LoadKML(path):
     sendKmlToLG(path + 'SST_regions.kml', path + 'slave.kml')
 
-def FlyTo(position):
-    pass
+def FlyTo(center_lat, center_lon):
+    sendFlyToToLG(center_lat, center_lon, 15000, 0, 0, 6000000, 2)
 
 def SouthAtlantic():
     LoadKML(global_vars.demo_files_path + 'South_Atlantic/')
+    FlyTo(-44.033173405198575, -18.55412927249652)
+    sleep(10)
 
 def Indian():
     LoadKML(global_vars.demo_files_path + 'Indian/')
+    FlyTo(-20.55819005127296, 71.46322969298124)
+    sleep(10)
 
 def WestPacific():
     LoadKML(global_vars.demo_files_path + 'West_Pacific/')
+    FlyTo(-3.6055350414132112, 148.72609606570853)
+    sleep(10)
 
 def EastPacific():
     LoadKML(global_vars.demo_files_path + 'East_Pacific/')
+    FlyTo(-14.22202896516345, -129.23682168130628)
+    sleep(10)
 
 def NorthAtlantic():
     LoadKML(global_vars.demo_files_path + 'North_Atlantic/')
+    FlyTo(43.090963600753945, -25.84386342158867)
+    sleep(10)
 
 def GenerateDemo():
     SouthAtlantic()
