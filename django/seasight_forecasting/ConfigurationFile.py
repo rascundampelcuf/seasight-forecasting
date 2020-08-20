df092145e6986e5df56bb556f51c88e4d3e5974f
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-08-20 21:24:08 +0200

    :pencil: Added fields in the configuration file

--- django/seasight_forecasting/ConfigurationFile.py
+++ django/seasight_forecasting/ConfigurationFile.py
@@ -24,6 +24,8 @@ def LoadConfigFile():
     global_vars.number_of_clusters = int(config['KML']['number_of_clusters'])
     global_vars.cmap = config['KML']['cmap']
     global_vars.sleep_in_thread = int(config['KML']['sleep_in_thread'])
+    global_vars.altitude = int(config['KML']['altitude'])
+    global_vars.pRange = int(config['KML']['range'])
 
     global_vars.server_IP = config['INSTALLATION']['server_IP']
     global_vars.lg_IP = config['INSTALLATION']['lg_IP']
@@ -32,6 +34,6 @@ def LoadConfigFile():
     global_vars.screen_for_colorbar = int(config['INSTALLATION']['screen_for_colorbar'])
     global_vars.project_location = config['INSTALLATION']['project_location']
     global_vars.logs = config['INSTALLATION']['logs']
-    global_vars.logs = config['INSTALLATION']['show_verbose']
+    global_vars.show_verbose = config['INSTALLATION']['show_verbose']
 
     print('Global variables loaded!')
