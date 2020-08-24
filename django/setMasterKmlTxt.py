bd787ba08869c86f2c712c09f050c15004ce0038
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-08-24 09:02:56 +0200

    :zap: Improved init files

--- django/setMasterKmlTxt.py
+++ django/setMasterKmlTxt.py
@@ -12,25 +12,25 @@ lg_pass = config['INSTALLATION']['lg_pass']
 screen_for_logos = int(config['INSTALLATION']['screen_for_logos'])
 
 kml = '<kml xmlns=\\\"http://www.opengis.net/kml/2.2\\\" xmlns:atom=\\\"http://www.w3.org/2005/Atom\\\" xmlns:gx=\\\"http://www.google.com/kml/ext/2.2\\\">'
-kml += '<Document>'
-kml += '<Folder>'
-kml += '<name>Logos</name>'
-kml += '<ScreenOverlay>'
-kml += '<name>Logo</name>'
-kml += '<Icon>'
-kml += '<href>http://{}:8000/static/logos/Logos.png</href>'.format(server_IP)
-kml += '</Icon>'
-kml += '<overlayXY x=\\\"0\\\" y=\\\"1\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
-kml += '<screenXY x=\\\"0.02\\\" y=\\\"0.9\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
-kml += '<rotationXY x=\\\"0\\\" y=\\\"0\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
-kml += '<size x=\\\"0.5\\\" y=\\\"0.5\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
-kml += '</ScreenOverlay>'
-kml += '</Folder>'
-kml += '</Document>'
-kml += '</kml>'
+kml += '\n ' + '<Document>'
+kml += '\n  ' + '<Folder>'
+kml += '\n   ' + '<name>Logos</name>'
+kml += '\n   ' + '<ScreenOverlay>'
+kml += '\n    ' + '<name>Logo</name>'
+kml += '\n    ' + '<Icon>'
+kml += '\n     ' + '<href>http://{}:8000/static/logos/Logos.png</href>'.format(server_IP)
+kml += '\n    ' + '</Icon>'
+kml += '\n    ' + '<overlayXY x=\\\"0\\\" y=\\\"1\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
+kml += '\n    ' + '<screenXY x=\\\"0.02\\\" y=\\\"0.98\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
+kml += '\n    ' + '<rotationXY x=\\\"0\\\" y=\\\"0\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
+kml += '\n    ' + '<size x=\\\"0.95\\\" y=\\\"0.3\\\" xunits=\\\"fraction\\\" yunits=\\\"fraction\\\"/>'
+kml += '\n   ' + '</ScreenOverlay>'
+kml += '\n  ' + '</Folder>'
+kml += '\n ' + '</Document>'
+kml += '\n' + '</kml>'
 
 logos_file_target = '/var/www/html/kml/slave_{}.kml'.format(screen_for_logos)
 
-command = "sshpass -p {} ssh {} echo \"{} > {}\"".format(lg_pass, lg_IP, kml, logos_file_target)
+command = "sshpass -p {} ssh {} echo \"'{}' > {}\"".format(lg_pass, lg_IP, kml, logos_file_target)
 print(command)
 os.system(command)
--- django/startDjango.py
+++ django/startDjango.py
@@ -7,6 +7,8 @@ configFilePath = r'app.conf'
 config.read(configFilePath)
 
 server_IP = config['INSTALLATION']['server_IP']
+project_location = config['INSTALLATION']['project_location']
 
+os.system('cd ~/{}Seasight-Forecasting/django').format(project_location)
 os.system('source venv/bin/activate')
 os.system('python manage.py runserver {}:8000'.format(server_IP))
