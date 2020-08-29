8b15928d6d6e9a3c9e8dcfb20e0e11115daa8b43
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-08-29 13:32:57 +0200

    :pencil: Improved init files

--- django/startDjango.py
+++ django/startDjango.py
@@ -9,6 +9,5 @@ config.read(configFilePath)
 server_IP = config['INSTALLATION']['server_IP']
 project_location = config['INSTALLATION']['project_location']
 
-os.system('cd ~/{}Seasight-Forecasting/django'.format(project_location))
 os.system('source venv/bin/activate')
 os.system('python manage.py runserver {}:8000'.format(server_IP))
